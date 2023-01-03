5a9b6382d6552c14a4785998fa4101a6c46bfa7d
unknown <swatikadu29.sk@gmail.com>
2023-01-04 01:02:33 +0530

    first commit

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,20 @@
+//values and variables
+const country = 'India'; 
+const continent = 'Asia';
+let population = '1.39 Billion';
+
+console.log(continent);
+console.log(country);
+console.log(population);
+
+// Datatypes
+let isIsland = false;
+let language;
+console.log(country);
+console.log(isIsland);
+console.log(language);
+
+// let, const, var
+language = 'Hindi';
+//country = 'China';
+
---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+let js = 'amazing';
+if (js === 'amazing')alert("JS is FUN!");
+
+console.log(29+27+29+31); 
+
+let firstName = 'Swati';
+let lastName = 'Kadu';
+
+console.log(firstName);
+console.log(lastName);
+
+let $function = 'add';
+let _count = 2;
+let hello$kitty = true;
+
+console.log($function);
+console.log(_count);
+console.log(hello$kitty);
